9d11728690fe0bf8d46fa46240487a3e8bdc7c75
pablo chesini <pchesini1@gmail.com>
2025-03-27 17:09:28 -0300

    Iniciando App

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView, Platform } from 'react-native';
+import { useState } from 'react';
+
+const colores = ["#F7DC6F","#A2D9CE","#D7BDE2"];
 
 export default function App() {
+
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [CurrentTime, setCurrentTime] = useState("POMO" |"CORTO"| "LARGO");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+    <View style={{paddingTop: Platform.OS === "android" && 30}}>
+      <Text style={styles.text}>Pomodoro</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  
+  text: {
+  fontSize:32,
+  fontWeigth: "bold"
+  }
 });
